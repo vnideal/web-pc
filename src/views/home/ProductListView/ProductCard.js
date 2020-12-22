e4ff585d734df17bf68ea52dbd77c8e28cecb479
nhannguyen <a4rum.vn@gmail.com>
2020-12-22 15:01:57 +0700

    update home page

--- src/views/home/ProductListView/ProductCard.js
+++ src/views/home/ProductListView/ProductCard.js
@@ -7,12 +7,19 @@ import {
   Card,
   CardContent,
   Divider,
-  Grid,
   Typography,
+  IconButton,
+  CardMedia,
+  CardHeader,
+  CardActions,
   makeStyles
 } from '@material-ui/core';
-import AccessTimeIcon from '@material-ui/icons/AccessTime';
-import GetAppIcon from '@material-ui/icons/GetApp';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import CommentIcon from '@material-ui/icons/Comment';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import getFromNow from 'src/utils/getFromNow';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,6 +32,30 @@ const useStyles = makeStyles((theme) => ({
   },
   statsIcon: {
     marginRight: theme.spacing(1)
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%' // 16:9
+  },
+  iconbutton: {
+    width: '33%',
+    padding: '4px 0px',
+    borderRadius: 4
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest
+    })
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)'
+  },
+  avatar: {
+  },
+  cardaction: {
+    padding: '4px 8px'
   }
 }));
 
@@ -32,82 +63,45 @@ const ProductCard = ({ className, product, ...rest }) => {
   const classes = useStyles();
 
   return (
-    <Card
-      className={clsx(classes.root, className)}
-      {...rest}
-    >
+    <Card className={clsx(classes.root, className)} {...rest}>
+      <CardHeader
+        avatar={(
+          <Avatar aria-label="recipe" className={classes.avatar} style={{ backgroundColor: product.user.avatar.color }}>
+            {product.user.avatar.letter}
+          </Avatar>
+        )}
+        action={(
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        )}
+        title={product.title}
+        subheader={getFromNow(product.createdAt)}
+      />
+      <CardMedia
+        className={classes.media}
+        image={product.media}
+        title="Paella dish"
+      />
+
       <CardContent>
-        <Box
-          display="flex"
-          justifyContent="center"
-          mb={3}
-        >
-          <Avatar
-            alt="Product"
-            src={product.media}
-            variant="square"
-          />
-        </Box>
-        <Typography
-          align="center"
-          color="textPrimary"
-          gutterBottom
-          variant="h4"
-        >
-          {product.title}
-        </Typography>
-        <Typography
-          align="center"
-          color="textPrimary"
-          variant="body1"
-        >
+        <Typography align="center" color="textPrimary" variant="body1">
           {product.description}
         </Typography>
       </CardContent>
       <Box flexGrow={1} />
       <Divider />
-      <Box p={2}>
-        <Grid
-          container
-          justify="space-between"
-          spacing={2}
-        >
-          <Grid
-            className={classes.statsItem}
-            item
-          >
-            <AccessTimeIcon
-              className={classes.statsIcon}
-              color="action"
-            />
-            <Typography
-              color="textSecondary"
-              display="inline"
-              variant="body2"
-            >
-              Updated 2hr ago
-            </Typography>
-          </Grid>
-          <Grid
-            className={classes.statsItem}
-            item
-          >
-            <GetAppIcon
-              className={classes.statsIcon}
-              color="action"
-            />
-            <Typography
-              color="textSecondary"
-              display="inline"
-              variant="body2"
-            >
-              {product.totalDownloads}
-              {' '}
-              Downloads
-            </Typography>
-          </Grid>
-        </Grid>
-      </Box>
+      <CardActions disableSpacing>
+        <IconButton className={classes.iconbutton} aria-label="favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton className={classes.iconbutton} aria-label="show more">
+          <CommentIcon />
+        </IconButton>
+        <IconButton className={classes.iconbutton} aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
     </Card>
   );
 };
--- src/views/home/ProductListView/data.js
+++ src/views/home/ProductListView/data.js
@@ -6,36 +6,64 @@ export default [
     createdAt: '27/03/2019',
     description:
       'Dropbox is a file hosting service that offers cloud storage, file synchronization, a personal cloud.',
-    media: '/static/images/products/product_1.png',
+    media: '/static/images/products/product_1.jpg',
     title: 'Dropbox',
-    totalDownloads: '594'
+    totalDownloads: '594',
+    user: {
+      name: 'Dropbox',
+      avatar: {
+        color: '#4caf50',
+        letter: 'D'
+      }
+    }
   },
   {
     id: uuid(),
     createdAt: '31/03/2019',
     description:
       'Medium is an online publishing platform developed by Evan Williams, and launched in August 2012.',
-    media: '/static/images/products/product_2.png',
+    media: '/static/images/products/product_2.jpg',
     title: 'Medium Corporation',
-    totalDownloads: '625'
+    totalDownloads: '625',
+    user: {
+      name: 'Medium Corporation',
+      avatar: {
+        color: '#cddc39',
+        letter: 'M'
+      }
+    }
   },
   {
     id: uuid(),
     createdAt: '03/04/2019',
     description:
       'Slack is a cloud-based set of team collaboration tools and services, founded by Stewart Butterfield.',
-    media: '/static/images/products/product_3.png',
+    media: '/static/images/products/product_3.jpg',
     title: 'Slack',
-    totalDownloads: '857'
+    totalDownloads: '857',
+    user: {
+      name: 'Slack',
+      avatar: {
+        color: '#ffc107',
+        letter: 'S'
+      }
+    }
   },
   {
     id: uuid(),
     createdAt: '04/04/2019',
     description:
       'Lyft is an on-demand transportation company based in San Francisco, California.',
-    media: '/static/images/products/product_4.png',
+    media: '/static/images/products/product_4.jpg',
     title: 'Lyft',
-    totalDownloads: '406'
+    totalDownloads: '406',
+    user: {
+      name: 'Lyft',
+      avatar: {
+        color: '#f44336',
+        letter: 'L'
+      }
+    }
   },
   {
     id: uuid(),
@@ -44,7 +72,14 @@ export default [
       'GitHub is a web-based hosting service for version control of code using Git.',
     media: '/static/images/products/product_5.png',
     title: 'GitHub',
-    totalDownloads: '835'
+    totalDownloads: '835',
+    user: {
+      name: 'GitHub',
+      avatar: {
+        color: '#9c27b0',
+        letter: 'G'
+      }
+    }
   },
   {
     id: uuid(),
@@ -53,6 +88,13 @@ export default [
       'Squarespace provides software as a service for website building and hosting. Headquartered in NYC.',
     media: '/static/images/products/product_6.png',
     title: 'Squarespace',
-    totalDownloads: '835'
+    totalDownloads: '835',
+    user: {
+      name: 'Squarespace',
+      avatar: {
+        color: '#f44336',
+        letter: 'S'
+      }
+    }
   }
 ];
