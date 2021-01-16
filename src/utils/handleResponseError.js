e64657425bac4391fc6ca6fabbfb285254614dd1
nhannguyen <a4rum.vn@gmail.com>
2021-01-16 16:02:28 +0700

    Format response errors

--- src/utils/handleResponseError.js
+++ src/utils/handleResponseError.js
@@ -1,3 +1,12 @@
+const formatResponseError = (responseData) => {
+  const { errors } = responseData;
+  Object.entries(errors).forEach(([key, value]) => {
+    errors[key] = value.join('<br>');
+  });
+
+  return responseData;
+};
+
 export default (error) => {
-  return error.response.data;
+  return formatResponseError(error.response.data);
 };
