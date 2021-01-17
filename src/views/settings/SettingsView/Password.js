222825cd6089dee643e57f82cb38222d0739038c
nhannguyen <a4rum.vn@gmail.com>
2021-01-17 18:25:27 +0700

    Update password logic

--- src/views/settings/SettingsView/Password.js
+++ src/views/settings/SettingsView/Password.js
@@ -1,83 +1,112 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import * as Yup from 'yup';
+import { Formik } from 'formik';
 import clsx from 'clsx';
-import {
-  Box,
-  Button,
-  Card,
-  CardContent,
-  CardHeader,
-  Divider,
-  TextField,
-  makeStyles
-} from '@material-ui/core';
+import { Box, Button, Card, CardContent, CardHeader, Divider, TextField, makeStyles } from '@material-ui/core';
+import ProfileService from 'src/services/profile/ProfileService';
 
-const useStyles = makeStyles(({
+const useStyles = makeStyles({
   root: {}
-}));
+});
 
 const Password = ({ className, ...rest }) => {
   const classes = useStyles();
-  const [values, setValues] = useState({
-    password: '',
-    confirm: ''
-  });
+  const navigate = useNavigate();
 
-  const handleChange = (event) => {
-    setValues({
-      ...values,
-      [event.target.name]: event.target.value
+  const handleSubmitForm = (values, { setSubmitting, setErrors }) => {
+    ProfileService.updatePassword(values.password, values.newPassword, values.confirmNewPassword).then((response) => {
+      setSubmitting(false);
+      if (response.result) {
+        localStorage.removeItem('currentUser');
+        navigate('/app/dashboard', { replace: true });
+      } else {
+        setErrors(response.errors);
+      }
     });
   };
 
   return (
-    <form
-      className={clsx(classes.root, className)}
-      {...rest}
+    <Formik
+      initialValues={{
+        password: '',
+        newPassword: '',
+        confirmNewPassword: ''
+      }}
+      validationSchema={Yup.object().shape({
+        password: Yup.string()
+          .max(255)
+          .required('Current Password is required'),
+        newPassword: Yup.string()
+          .max(255)
+          .required('New password is required'),
+        confirmNewPassword: Yup.string()
+          .max(255)
+          .required('Confirm new password is required')
+          .when('newPassword', {
+            is: (val) => !!(val && val.length > 0),
+            then: Yup.string().oneOf([Yup.ref('newPassword')], 'Both new password need to be the same')
+          })
+      })}
+      onSubmit={(values, { setSubmitting, setErrors }) => handleSubmitForm(values, { setSubmitting, setErrors })}
     >
-      <Card>
-        <CardHeader
-          subheader="Update password"
-          title="Password"
-        />
-        <Divider />
-        <CardContent>
-          <TextField
-            fullWidth
-            label="Password"
-            margin="normal"
-            name="password"
-            onChange={handleChange}
-            type="password"
-            value={values.password}
-            variant="outlined"
-          />
-          <TextField
-            fullWidth
-            label="Confirm password"
-            margin="normal"
-            name="confirm"
-            onChange={handleChange}
-            type="password"
-            value={values.confirm}
-            variant="outlined"
-          />
-        </CardContent>
-        <Divider />
-        <Box
-          display="flex"
-          justifyContent="flex-end"
-          p={2}
-        >
-          <Button
-            color="primary"
-            variant="contained"
-          >
-            Update
-          </Button>
-        </Box>
-      </Card>
-    </form>
+      {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
+        <form className={clsx(classes.root, className)} onSubmit={handleSubmit} {...rest}>
+          <Card>
+            <CardHeader subheader="Update password" title="Password" />
+            <Divider />
+            <CardContent>
+              <TextField
+                error={Boolean(touched.password && errors.password)}
+                fullWidth
+                helperText={touched.password && errors.password}
+                label="Password"
+                margin="normal"
+                name="password"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                type="password"
+                value={values.password}
+                variant="outlined"
+              />
+              <TextField
+                error={Boolean(touched.newPassword && errors.newPassword)}
+                fullWidth
+                helperText={touched.newPassword && errors.newPassword}
+                label="New password"
+                margin="normal"
+                name="newPassword"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                type="password"
+                value={values.newPassword}
+                variant="outlined"
+              />
+              <TextField
+                error={Boolean(touched.confirmNewPassword && errors.confirmNewPassword)}
+                fullWidth
+                helperText={touched.confirmNewPassword && errors.confirmNewPassword}
+                label="Confirm New password"
+                margin="normal"
+                name="confirmNewPassword"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                type="password"
+                value={values.confirmNewPassword}
+                variant="outlined"
+              />
+            </CardContent>
+            <Divider />
+            <Box display="flex" justifyContent="flex-end" p={2}>
+              <Button color="primary" variant="contained" type="submit" disabled={isSubmitting}>
+                Update
+              </Button>
+            </Box>
+          </Card>
+        </form>
+      )}
+    </Formik>
   );
 };
 
